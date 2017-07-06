8f733f6a79d44dcf089263773d98cbdb4d90a9e5
VC1995 <vishesh1995@gmail.com>
2017-07-07 00:50:25 +0530

    much better outgoing.py

--- ensimesublime/outgoing.py
+++ ensimesublime/outgoing.py
@@ -1,33 +1,73 @@
+import json
+
+
 class RpcRequest(object):
-    def run_in(self, env):
+
+    def send_request(self, request, client, async):
+        """Send a request to the server."""
+        client.env.logger.debug('send_request: in')
+
+        message = {'callId': client.call_id, 'req': request}
+        client.call_options[client.call_id] = {'async': async}
+        client.call_options[client.call_id].update(self.call_options())
+        client.env.logger.debug('send_request: %s', message)
+        client.send(json.dumps(message))
+
+        call_id = client.call_id
+        client.call_id += 1
+        return call_id
+
+    def run_in(self, env, async=False):
+        self.send_request(self.json_repr(), env.client, async)
+
+    def json_repr(self):
         raise NotImplementedError
+
+    def call_options(self):
+        return None
 
 
 class ConnectionInfoRequest(RpcRequest):
-    def run_in(self, env):
-        call_id = env.client.send_request({"typehint": "ConnectionInfoReq"})
-        return call_id
+    def __init__(self):
+        super(ConnectionInfoRequest, self).__init__()
+
+    def json_repr(self):
+        return {"typehint": "ConnectionInfoReq"}
 
 
 class TypeCheckFilesReq(RpcRequest):
     def __init__(self, filenames):
+        super(TypeCheckFilesReq, self).__init__()
         self.filenames = list(filenames)
 
-    def run_in(self, env):
-        call_id = env.client.send_request(
-            {"typehint": "TypecheckFilesReq",
-             "files": self.filenames})
-        return call_id
+    def json_repr(self):
+        return {"typehint": "TypecheckFilesReq",
+                "files": self.filenames}
 
 
 class SymbolAtPointReq(RpcRequest):
     def __init__(self, filename, pos):
+        super(SymbolAtPointReq, self).__init__()
         self.filename = filename
         self.pos = pos
 
-    def run_in(self, env):
-        call_id = env.client.send_request({
-            "point": self.pos,
-            "typehint": "SymbolAtPointReq",
-            "file": self.filename})
-        return call_id
+    def json_repr(self):
+        return {"point": self.pos,
+                "typehint": "SymbolAtPointReq",
+                "file": self.filename}
+
+
+class ImportSuggestionsReq(RpcRequest):
+    def __init__(self, pos, file, word, max_results=10):
+        super(ImportSuggestionsReq, self).__init__()
+        self.pos = pos
+        self.file = file
+        self.names = [word]
+        self.max_results = max_results
+
+    def json_repr(self):
+        return {"point": self.pos,
+                "maxResults": self.maxResults,
+                "names": self.names,
+                "typehint": "ImportSuggestionsReq",
+                "file": self.file}
