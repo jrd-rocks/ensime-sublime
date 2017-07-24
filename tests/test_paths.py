8cbc00672301b292e8adfe9fba7c75349068ae01
Jeroen <jeroen@jrd.rocks>
2017-07-24 13:36:54 +0200

    cleaner extraction of root from path, no ext dependacies, with basic tests

---
+++ tests/test_paths.py
@@ -0,0 +1,14 @@
+import os,  paths
+
+if os.name == "nt":
+	def test_drive_path():
+	    assert paths.root_as_str_from_abspath('Z:\\thisis\\a\\path') == 'z:\\'
+
+	def test_drive_path():
+	    assert paths.root_as_str_from_abspath('Z:/some/6546/path/thing.scala') == 'z:\\'
+
+	def test_network_path():
+	    assert paths.root_as_str_from_abspath('//STORAGE/misc/server/a/b/c') == '\\\\storage\\misc'
+else:
+	def test_drive_path():
+	    assert paths.root_as_str_from_abspath('/this/is/a/path') == '/'
